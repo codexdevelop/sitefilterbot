973188b78e5fd6e08791735d216fb3712d59d15c
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-08 12:53:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -180,7 +180,7 @@ async def next_page(bot, query):
         for file_num, file in enumerate(files, start=offset+1):
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}</a></b>"""
     else: 
-        BLOGSPOT_URL = "https://your-blogspot.com/p/redirect.html"
+        BLOGSPOT_URL = "https://technoji786.blogspot.com/2025/02/codex-channel.html"
         btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)} ≽ {formate_file_name(file.file_name)}",url=f"{BLOGSPOT_URL}?file={file.file_id}")] 
 	       for file in files
 	      ]
@@ -1475,7 +1475,7 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
         for file_num, file in enumerate(files, start=1):
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start={"pm_mode_" if pm_mode else ''}file_{ADMINS[0] if pm_mode else message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {formate_file_name(file.file_name)}</a></b>"""
     else:    
-        BLOGSPOT_URL = "https://your-blogspot.com/p/redirect.html"
+        BLOGSPOT_URL = "https://technoji786.blogspot.com/2025/02/codex-channel.html"
         btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)} ≽ {formate_file_name(file.file_name)}",url=f"{BLOGSPOT_URL}?file={file.file_id}")] 
 	       for file in files
 	      ]
